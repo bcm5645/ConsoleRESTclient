8714c61c60fda2e073f8576edcd5ccebe89cf5e8
Wiktor Marczak <v460gle@wp.pl>
2020-05-26 00:28:02 +0200

    Dodanie weryfikacji NIP w bialej ksiedze

--- ConsoleRESTclient/Program.cs
+++ ConsoleRESTclient/Program.cs
@@ -13,18 +13,27 @@ namespace ConsoleRESTclient
 
 		static async Task Main(string[] args)
 		{
-			var repos = await ProcessRepositories();
-			foreach (var repo in repos)
-			{
-				Console.WriteLine(repo.Name);
-				Console.WriteLine(repo.Description);
-				Console.WriteLine(repo.GitHubHomeUrl);
-				Console.WriteLine(repo.Homepage);
-				Console.WriteLine(repo.Watchers);
-				Console.WriteLine(repo.LastPushUTC);
-				Console.WriteLine(repo.LastPush);
-				Console.WriteLine();
-			}
+			ProcessWhitelist();
+			Console.WriteLine("Oczekiwanie na przetworzenie żądania przez MF");
+			//foreach(var wl in whitelist)
+			//{
+
+			//}
+
+
+			//var repos = await ProcessRepositories();
+			//foreach (var repo in repos)
+			//{
+			//	Console.WriteLine(repo.Name);
+			//	Console.WriteLine(repo.Description);
+			//	Console.WriteLine(repo.GitHubHomeUrl);
+			//	Console.WriteLine(repo.Homepage);
+			//	Console.WriteLine(repo.Watchers);
+			//	Console.WriteLine(repo.LastPushUTC);
+			//	Console.WriteLine(repo.LastPush);
+			//	Console.WriteLine();
+			//}
+			Console.ReadKey();
 		}
 
 		private static async Task<List<Repository>> ProcessRepositories()
@@ -33,15 +42,29 @@ namespace ConsoleRESTclient
 			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
 			client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
 
-
-			//var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
 			var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+
 			return await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
 		}
 
+		private static async void ProcessWhitelist()
+		{
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
+			Console.WriteLine("Podaj NIP do zweryfikowania");
+			var nip = Console.ReadLine().Replace("-","");
 
+			var stringTask = client.GetStreamAsync($"https://wl-api.mf.gov.pl/api/search/nip/{nip}?date=2020-05-25");
+			var wl = await JsonSerializer.DeserializeAsync<VatWhiletList>(await stringTask);
+			if (wl != null)
+			{
+				Console.WriteLine(wl.Result.resultSubject.name);
+				Console.WriteLine(wl.Result.resultSubject.workingAddress);
+				Console.WriteLine(wl.Result.resultSubject.nip);
+				Console.WriteLine(wl.Result.resultSubject.regon);
+				Console.WriteLine(wl.Result.resultSubject.statusVat);
+			}
+		}
 	}
-
-
 }
---
+++ ConsoleRESTclient/VatWhiletList.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ConsoleRESTclient
+{
+	class VatWhiletList
+	{
+		[JsonPropertyName("result")]
+		public VatWhiteListResult Result { get; set; } = new VatWhiteListResult();
+	}
+
+	class VatWhiteListResult
+	{
+		[JsonPropertyName("subject")]
+		public ResultSubject resultSubject { get; set; } = new ResultSubject();
+	}
+
+	class ResultSubject
+	{
+		[JsonPropertyName("name")]
+		public string name { get; set; }
+
+		[JsonPropertyName("nip")]
+		public string nip { get; set; }
+
+		[JsonPropertyName("statusVat")]
+		public string statusVat { get; set; }
+
+		[JsonPropertyName("regon")]
+		public string regon { get; set; }
+
+		[JsonPropertyName("workingAddress")]
+		public string workingAddress { get; set; }
+	}
+}
+
+
+/*
+result
+subject
+name	"FIRMA HANDLOWO-USŁUGOWA 'VICTOR' S.C., WIESŁAW ŚLIPEK, DOROTA ŚLIPEK"
+nip	"8291490928"
+statusVat	"Czynny"
+regon	"730365570"
+pesel	null
+krs	null
+residenceAddress	null
+workingAddress	"TYMIENICE 42A, 98-220 TYMIENICE"
+representatives[]
+authorizedClerks[]
+partners[]
+registrationLegalDate   "1998-03-07"
+registrationDenialBasis	null
+registrationDenialDate	null
+restorationBasis	null
+restorationDate	null
+removalBasis	null
+removalDate	null
+accountNumbers	
+0	"64124033051111000029376791"
+hasVirtualAccounts	false
+requestId	"94jjd-87hh832"
+requestDateTime	"25-05-2020 23:27:38"
+*/
+
+
+
