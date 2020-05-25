f1def01fa740199706ec09c2d936833eb52fd2e2
WiktorM <v460gle@wp.pl>
2020-05-25 18:45:23 +0200

    Initial commit

---
+++ ConsoleRESTclient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace ConsoleRESTclient
+{
+	class Program
+	{
+		private static readonly HttpClient client = new HttpClient();
+
+		static async Task Main(string[] args)
+		{
+			var repos = await ProcessRepositories();
+			foreach (var repo in repos)
+			{
+				Console.WriteLine(repo.Name);
+				Console.WriteLine(repo.Description);
+				Console.WriteLine(repo.GitHubHomeUrl);
+				Console.WriteLine(repo.Homepage);
+				Console.WriteLine(repo.Watchers);
+				Console.WriteLine(repo.LastPushUTC);
+				Console.WriteLine(repo.LastPush);
+				Console.WriteLine();
+			}
+		}
+
+		private static async Task<List<Repository>> ProcessRepositories()
+		{
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+			client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+
+			//var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+			var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+			return await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+		}
+
+
+
+	}
+
+
+}
---
+++ ConsoleRESTclient/Repository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ConsoleRESTclient
+{
+	class Repository
+	{
+		[JsonPropertyName("name")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("html_url")]
+		public Uri GitHubHomeUrl { get; set; }
+
+		[JsonPropertyName("homepage")]
+		public Uri Homepage { get; set; }
+
+		[JsonPropertyName("watchers")]
+		public int Watchers { get; set; }
+
+		[JsonPropertyName("description")]
+		public string Description{ get; set; }
+
+		[JsonPropertyName("pushed_at")]
+		public DateTime LastPushUTC{ get; set; }
+
+		public DateTime LastPush => LastPushUTC.ToLocalTime();
+	}
+}
